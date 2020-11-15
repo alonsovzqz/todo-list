472312b2adfe84a6d8e066cbc982704fadcc39ee
Alonso Vazquez <haloanderson45@gmail.com>
2020-11-15 03:20:18 -0600

    Fix issue for deleting function.

--- src/App.js
+++ src/App.js
@@ -37,7 +37,7 @@ const App = () => {
     axios
       .delete(`https://jsonplaceholder.typicode.com/todos/${id}`)
       .then((res) =>
-        setTodos([...todos.todos.filter((todo) => todo.id !== id)])
+        setTodos([...todos.filter((todo) => todo.id !== id)])
       );
   };
 
